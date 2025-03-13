92ffcef6437098ab59f2733dfd13d77a0d90ab5b
tyler <tyler_floyd@live.com>
2025-03-13 12:52:57 -0700

    init chat history component

--- frontend/src/api/index.js
+++ frontend/src/api/index.js
@@ -2,7 +2,7 @@
 
 var socket = new WebSocket("ws://localhost:8080/ws");
 
-let connect = () => {
+let connect = (cb) => {
     console.log("Connecting to server...");
 
     socket.onopen = () => {
@@ -11,6 +11,7 @@ let connect = () => {
 
     socket.onmessage = (msg) => {
         console.log(msg);
+        cb(msg);
     };
 
     socket.onclose = (event) => {
